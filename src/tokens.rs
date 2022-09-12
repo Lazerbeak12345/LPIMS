250e05cf68f2851ed5339515d24146ce98f05ac6
Nathan Fritzler <nfblaster@live.com>
2022-09-11 20:41:21 -0600

    Start actual work on regexes

--- src/tokens.rs
+++ src/tokens.rs
@@ -1,16 +1,41 @@
-fn get_initial_regexes() {
-    todo!("compile initial regular expressions");
-    todo!("The list of named regexes is dynamic, and can be modified during the
-          parse phase.");
+use regex::{Regex, Error as ReError};
+
+/// A very slow regex::RegexSet that lets you change the sets on the fly
+type RegexVec = Vec<Regex>;
+macro_rules! regexVecUncompiled {
+    ( $( $x:expr ),* ) => {
+        vec![
+            $(
+                Regex::new($x)
+            ),*
+        ]
+    };
+}
+macro_rules! regexVec {
+    ( $( $x:expr ),* ) => {
+        regexVecUncompiled![
+            $( $x ),*
+        ].into_iter().map(|reg:Result<Regex,ReError>| reg.unwrap()).collect()
+    };
+}
+
+/** Returns the "default" regexes.
+ *
+ * TODO test that The list of named regexes is dynamic, and can be modified during the parse phase.
+ */
+fn get_initial_regexes() -> RegexVec {
+    todo!("Fill out token list");
+    regexVec![]
 }
 
 /// Stream token sequence from input file
-pub fn get_token_stream(file: String) {
+pub fn get_token_stream(data: String) {
     get_initial_regexes();
-    todo!("load file `{}` into buffer to be operated upon", file);
+    todo!("do something with '{}'", data);
     {
         todo!("Use current regexps to get next token");
         todo!("Tokens are granted upon request.");
+        todo!("Tokens include location and path data.");
         todo!("Can be told to skip.");
     }
 }
